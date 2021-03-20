6fc875313b02bce0fc0d81af01ef0ba1f737f208
Md Taha Reyaz <78434689+PyDev-Taha@users.noreply.github.com>
2021-03-20 18:30:36 +0530

    Update Basic Calculator.py

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -3,17 +3,17 @@ op=input("Enter operator:")
 num2=float(input("Enter second number:"))
 
 if op== "+":
-    print("Sum is", num1 + num2)
+    print( num1 + num2)
 elif op== "-":
-    print("Subtraction is", num1 - num2)
+    print( num1 - num2)
 elif op=="*":
-    print("Multiplication is", num1 * num2)
+    print( num1 * num2)
 elif op== "/":
-    print("Division is ", num1 / num2)
+    print( num1 / num2)
 elif op=="%":
-    print(num1 % num2)
+    print( num1 % num2)
 elif op=="**":
-    print(num1 ** num2)
+    print( num1 ** num2)
 
 else:
     print("Invalid Operator")
